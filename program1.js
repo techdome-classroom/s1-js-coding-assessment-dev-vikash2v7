6d40b7a2bc5f7e17d623c98143d4159d66a14187
dev-vikash2v7 <vikashvermacom92@gmail.com>
2024-09-30 12:17:12 +0530

    update

--- program1.js
+++ program1.js
@@ -3,9 +3,33 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+
+    let char_stack = [];
+
+    const char_map = {
+        ']': '[',
+        '}': '{',
+        ')': '(',
+    };
+
+    for (let char of s) {
+        if (char_map[char]) {
+            let top_element = char_stack.length === 0 ? '#' : char_stack.pop();
+
+            if (top_element !== char_map[char]) {
+                return false;
+            }
+        } else {
+            char_stack.push(char);
+        }
+    }
+
+    return char_stack.length === 0;
     
 };
 
 module.exports = { isValid };
+
+
 
 
--- program2.js
+++ program2.js
@@ -3,6 +3,33 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+
+    const roman_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    
+    let total = 0;
+    let prev = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        const current = roman_map[s[i]];
+        
+        if (current > prev) {
+            total += current - 2 * prev;
+        } else {
+            total += current;
+        }
+        
+        prev = current;
+    }
+    
+    return total;
     
 };
 
